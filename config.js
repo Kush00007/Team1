e43511b23b8dbe760901c841898038cb04131133
Kush00007 <87892026+Kush00007@users.noreply.github.com>
2022-07-10 12:00:45 +0530

    Changed color of button

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyCYyqItM8ep3QFy2u0sdO3xa03REuwTIUQ",
+    authDomain: "team-voting-b5bfe.firebaseapp.com",
+    databaseURL: "https://team-voting-b5bfe-default-rtdb.firebaseio.com",
+    projectId: "team-voting-b5bfe",
+    storageBucket: "team-voting-b5bfe.appspot.com",
+    messagingSenderId: "578123667195",
+    appId: "1:578123667195:web:b67213624e55016370825e"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
